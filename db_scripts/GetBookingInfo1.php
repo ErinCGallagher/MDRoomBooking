b3c238499c24c624c0998948fb7ab03cdc3c731e
Lex-Luthor <lexi.flynn@queensu.ca>
2015-11-20 14:46:54 -0500

    Create GetBookingInfo1.php

---
+++ db_scripts/GetBookingInfo1.php
@@ -0,0 +1,20 @@
+<?php
+		$data = json_decode(file_get_contents("php://input"));
+
+		$BID = mysql_real_escape_string($data->BookingID);
+
+		$host = "localhost";
+		$user = "root";
+		$password = "";
+		$database = "mdroombooking";
+
+		$cxn = mysqli_connect($host,$user,$password, $database);
+		// Check connection
+		if (mysqli_connect_errno()){
+  			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  			die();
+  		}
+
+   		$query3 = "SELECT * FROM Bookings WHERE bookingID = '$BID';"
+		$bookingInfo = my_sqli_query($cxn, $query3);
+?>
