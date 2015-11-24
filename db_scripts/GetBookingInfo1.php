a6829e43416dbfe542c5243e3705fb1fe2b2045d
Laura Brooks <11lmb23@queensu.ca>
2015-11-24 14:05:32 -0500

    Merge fixes on php files

--- db_scripts/GetBookingInfo1.php
+++ db_scripts/GetBookingInfo1.php
@@ -17,6 +17,12 @@
 
    		$query3 = "SELECT * FROM Bookings WHERE bookingID = '$BID';"
 		$bookingInfo = my_sqli_query($cxn, $query3);
-		
-		$json = json_encode($bookingInfo);
+
+
+		// convert to json
+        $json = json_encode( $bookingInfo);
+
+		// echo the json string
+        echo $json;
+
 ?>
