f30845b09cd1f55817394714d0681305377c04ee
Lex-Luthor <lexi.flynn@queensu.ca>
2015-11-25 18:31:25 -0500

    Fixed 30 minute booking bug and added comments

--- db_scripts/GetDaily1.php
+++ db_scripts/GetDaily1.php
@@ -1,37 +1,43 @@
 <?php
-
-
-		$data = json_decode(file_get_contents("php://input"));
-
-		$room = mysql_real_escape_string($data->Room);
-		$nonFormattedDay = mysql_real_escape_string($data->Date);
-
+		
+		//Database connection variables
 		$host = "localhost";
 		$user = "root";
 		$password = "";
 		$database = "mdroombooking";
-
-    		$day = strtotime($nonFormattedDay);
-    		$day = date('Y-m-d', $day);
-
+		//Connent to database
 		$cxn = mysqli_connect($host,$user,$password, $database);
 		// Check connection
 		if (mysqli_connect_errno()){
   			echo "Failed to connect to MySQL: " . mysqli_connect_error();
   			die();
   		}
-   		
-   		//get daily bookings
+  		//Get post data stream 
+		$data = json_decode(file_get_contents("php://input"));
+		//Get parameters from 
+		$room = mysql_real_escape_string($data->Room);
+		$nonFormattedDay = mysql_real_escape_string($data->Date);
+		//Format day
+    		$day = strtotime($nonFormattedDay);
+    		$day = date('Y-m-d', $day);
+
+   		//get daily bookings from database
    		$query1 = "SELECT Bookings.BookingID, BookingSlots.BlockID, UID, BookingDate, BookingSlots.RoomID, StartTime, EndTime, Reason, COUNT(*) as numBlocks FROM Bookings JOIN BookingSlots JOIN Blocks ON Bookings.BookingID = BookingSlots.BookingID AND BookingSlots.BlockID = Blocks.BlockID WHERE RoomID = '$room' AND BookingDate = '$day' GROUP BY BookingID ASC;";
       		$dailyBookings = mysqli_query($cxn, $query1);
-   		date_default_timezone_set('America/New_York');
+   		
    		$result = array();
+   		
+   		//Set timezone
+   		date_default_timezone_set('America/New_York');
+   		
+   		//Loop through each returned row 
    		while ($row = mysqli_fetch_assoc($dailyBookings)) {
    			
-   			//get number of blocks
+   			//Get number of blocks
    			$numBlocks = $row['numBlocks'];
    			
-   			//add thirty minutes for each block
+   			//Add thirty minutes to start time for each 
+   			//block in booking if there is more than one
    			if ($numBlocks != 1) {
    			$startTime = $row['StartTime'];
    			$endTime =  strtotime($startTime);
@@ -44,12 +50,14 @@
    			
    			//change the endtime to appropriate value
    			$row['EndTime'] = date("H:i:s", $endTime);
-   			$result[] = $row;
    			}
+   		//Add row to result 
+   		$result[] = $row;
    		}
-   		
+   		//encode result to json format
    		$json = json_encode($result);
    		echo $json;
-  
+  		
+  		//Close the database connection
    		mysqli_close($cxn);
 ?>
