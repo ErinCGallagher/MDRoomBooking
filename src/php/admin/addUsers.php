54b6cac98f32c5c18d7ee2bb28e865d3f20175e2
shannonklett <shannonklett@users.noreply.github.com>
2016-01-17 17:13:27 -0500

    Upload file to database

--- src/php/admin/addUsers.php
+++ src/php/admin/addUsers.php
@@ -1,1 +1,42 @@
+<?php
 
+	$save_dir = "../../../user_files/";
+	// $_FILES is a global variable that gets the files from HTTP POST
+	$target_file = $save_dir . basename($_FILES["fileToUpload"]["name"]);
+	$uploadOk = 1;
+	$fileType = pathinfo($target_file,PATHINFO_EXTENSION);
+	
+	// Check file selected
+	if($_FILES["fileToUpload"]["tmp_name"] = 0) {
+        echo "No file selected.";
+        $uploadOk = 0;
+	}
+
+	// Check if file already exists
+	if (file_exists($target_file)) {
+	    echo "Sorry, file already exists.";
+	    $uploadOk = 0;
+	}
+	
+	// Check file size
+	if ($_FILES["fileToUpload"]["size"] > 500000) {
+	    echo "Sorry, your file is too large.";
+	    $uploadOk = 0;
+	}
+	// Allow certain file formats
+	if($fileType != "csv") {
+	    echo "User List must be a .csv file.";
+	    $uploadOk = 0;
+	}
+	// Check if $uploadOk is set to 0 by an error
+	if ($uploadOk == 0) {
+	    echo "Sorry, your file was not uploaded.";
+	// if everything is ok, try to upload file
+	} else {
+	    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+	        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+	    } else {
+	        echo "Sorry, there was an error uploading your file.";
+	    }
+	}
+?>
