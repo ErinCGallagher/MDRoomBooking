8de6c4db67469812e3b2c21b49e1cd202d3989e1
Laura Brooks <11lmb23@queensu.ca>
2016-01-17 18:56:54 -0500

    Created a file with the PDO connection to DB, backend check for conflicts

---
+++ src/php/connection.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "mdroombooking";
+
+$db = new PDO('mysql:host=' . $host . ';dbname=' . $database . ';charset=utf8',  'root', '');
+	
+?>
