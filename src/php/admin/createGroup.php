a5b9ed64a40c547f838b287827b1742d9004c8d5
Lex-Luthor <lexi.flynn@queensu.ca>
2016-01-18 16:42:33 -0500

    added form

--- src/php/admin/createGroup.php
+++ src/php/admin/createGroup.php
@@ -12,19 +12,39 @@
 	$data = json_decode(file_get_contents("php://input"));
 	//Get parameters from 
 	$groupName = $data->groupName;
-	$hours = $data->hours;
-	// $hoursPerSemester = $data->hoursPerSemester
-	// $fall = $data->fall;
-	// $winter = $data->winter;
-	//$summer = $data->summer);
+	$hoursPerWeek = $data->hours;
+	$totalHoursPerSem = $data->toatalHoursPerSem;
+	$totalHoursPerTime = $data->totalHoursPerTime;
+	$hasBookingDurationRestriction = $data->hasBookingDurationRestriction;
+	$fall = $data->fall;
+	$winter = $data->winter;
+	$summer = $data->summer;
+	$startDate = $data->startDate;
+	$endDate = $data->endDate;
 
+	$year = ('Y');
 
-	$query = "INSERT INTO UGroups(groupName, hours) VALUES ('$groupName' , '$hours')";
-	$stmt = $db->query($query);
-
-	$groupID = $db->lastInsertId();
-
-	// don't need to encode value
-	echo $groupID
+	$query = "INSERT INTO UGroups(groupName, addHrsType, hours, startDate, endDate, academicYr) VALUES ('$groupName' , '$addHrsType' , '$hours' , '$startDate', '$endDate', '$academicYr')";
+ 	$stmt = $db->query($query);
+ 	$groupID = $db->lastInsertId();
+ 
+	echo $groupID;
    
+   ///////////////// new script addUsers.php
+   
+   
+ 	// Get the users list in the new group 
+ 	$userList = array("12af", "13eg", "14sk", "15lb");
+  
+
+ 	// Update the permissions of each member of the group
+ 	foreach($userList as $user) {
+		$query = "INSERT INTO Permission (uID, groupId, acdemicYr) VALUES ('$user', '$groupID', '$year')";
+		$stmt = $db->query($query);
+		//Give
+		//user is not in user table??
+		$query = "UPDATE User SET addHrs = addHrs + '$hours' WHERE uID = '$user'";
+		$stmt = $db->query($query);
+  	}
+
 ?>
