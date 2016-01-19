d0d3da3c1ceb2bba0112cac8533ff19c143841df
shannonklett <shannonklett@users.noreply.github.com>
2016-01-18 19:07:26 -0500

    Users in file get added to group

--- src/php/admin/addUsers.php
+++ src/php/admin/addUsers.php
@@ -10,24 +10,38 @@
 	// reads in file and stores contents in $contents
 	require '../uploadFile.php';
 
-	$groupID = 1;
+	// NEED TO GET THESE VALUES from frontend
+	$groupID = 2; 
 	$year = 2016;
 	
 	//add users to group
 	$valueString = "";
 
-	foreach ($contents as $line) {
+	foreach ($contents as $user) {
 		//want format ('uID', 'groupID', 'year'),
-		echo("LINE\n".$line);
-		$valueString .= "('" . $line . "', '" . $groupID . "', '" . $year . "'),\n";
+		
+		// need to check that user doesn't already have permission!!!!
+		$valueString .= "('" . $user . "', '" . $groupID . "', '" . $year . "'),\n";
 	}
 
+	$valueString = chop($valueString, ",\n"); //remove extra characters
 	echo $valueString;
+
+	// Update the permissions of each member of the group
+ 	// foreach($contents as $user) {
+		// $query = "INSERT INTO Permission (uID, groupId, acdemicYr) VALUES ('$user', '$groupID', '$year')";
+		// $stmt = $db->query($query);
+		// //Give
+		// //user is not in user table??
+		// $query = "UPDATE User SET addHrs = addHrs + '$hours' WHERE uID = '$user'";
+		// $stmt = $db->query($query);
+  // 	}
+
 
 	$query = "INSERT INTO Permission (uID, groupID, academicYr) VALUES $valueString";
 	$stmt = $db->query($query);
 
 	// return if insert was successful or not
-	echo $stmt
+	//echo ("RESULT ".$stmt);
 
 ?>
