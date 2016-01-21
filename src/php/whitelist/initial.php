50ac6f464ce20fcde8441df154d3e2072bb8dff4
Laura Brooks <11lmb23@queensu.ca>
2016-01-21 14:55:04 -0500

    Initial script getting buildings and rooms

--- src/php/whitelist/initial.php
+++ src/php/whitelist/initial.php
@@ -1,10 +1,45 @@
 <?php
 session_start();
 
+include('../connection.php');
+
 //Get all buildings
+$sth = $db->prepare("SELECT * FROM Building");
+$sth->execute();
+$rows = $sth->fetchAll();
 
+$allBuildings = array();
 
-//Get all rooms
+//Put result in an array 
+foreach($rows as $row){ 
+	$building = $row['buildingID'];
+	$allBuildings[$building] = array();
+	$allBuildings[$building]['openTime'] = $row['openTime'];
+	$allBuildings[$building]['closeTime'] = $row['closeTime'];
+	
+	//get all rooms for building
+	$allBuildings[$building]['rooms'] = array();
+	$sth = $db->prepare("SELECT roomID FROM Rooms WHERE building = ?");
+	$sth->execute(array($building));
+	$rooms = $sth->fetchAll();
+	foreach($rooms as $room){ 
+		$allBuildings[$building]['rooms'][] = $room['roomID'];
+	}	
+}
+
+$db = NULL;
+
+$_SESSION["buildings"] = $allBuildings;
+
+/*
+foreach($allBuildings as $building => $info){
+	echo "Building: " . $building . " Open: " . $info['openTime'] . " Close: " . $info['closeTime'] . "<br>";
+	echo "Rooms: ";
+	foreach ($info['rooms'] as $room){
+		echo $room . "<br>";
+	}
+}
+*/
 
 
 ?>
