83f3b083d660ed3e7530fe8eb2ffd1fd120cb66c
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2016-01-25 00:29:37 -0500

    merge conflict that didn't get fixed from multi_room

--- src/php/bookings/createBooking.php
+++ src/php/bookings/createBooking.php
@@ -1,4 +1,5 @@
 <?php
+	session_start();
 
 	//Database connection
 	include('../connection.php');
@@ -11,84 +12,95 @@
 	$data = json_decode(file_get_contents("php://input"));
 
 	//Set parameters from datastream
-	$UID = $data->UID;
-	$Room = $data->RoomID;
-	$Reason = $data->Reason;
-	$Desc = $data->OtherDesc;
+	$uID = $data->uID;
+	$room = $data->roomID;
+	$reason = $data->reason;
+	$building = $data->building;
+	$desc = $data->otherDesc;
 	$year = "2015/2016";
 	$numP = $data->numParticipants;
 	$localStart = $data->start;
 	$localEnd = $data->end;
-
-	$utcStart = strtotime($localStart);
-	$startDate = date('Y-m-d', $utcStart);
-	$startTime = date('H:i:s', $utcStart);
-
-	$utcEnd = strtotime($localEnd);
-	$endDate = date('Y-m-d', $utcEnd);
-	$endTime = date('H:i:s', $utcEnd);
-
-	//Array to hold blocks 
-	$blocks = [];
 	
-	$sth = $db->prepare("SELECT blockID, endTime FROM Blocks WHERE startTime = ?");
-	$sth->execute(array($startTime));
-	$rows = $sth->fetchAll();
+	include('../whitelist/checkBooking.php');
 	
-	foreach($rows as $row) {
-		$block = $row['blockID'];
-		$blocks[] = $block;
-		$blockend = $row['endTime'];
-	}
+	$allBuildings = $_SESSION["buildings"];
 	
-	//Get all blocks for booking 
-	$totalB = 1;
-	while ($blockend != $endTime){
-		$block = $block + 1;
-		$totalB = $totalB + 1;
-		$blocks[] = $block;
-		
-		$sth = $db->prepare("SELECT endTime FROM Blocks WHERE blockID = ?");
-		$sth->execute(array($block));
+	if (!in_array($reason, $reasonsList)) {
+		http_response_code(403); //Invalid Entry
+	} else if (!in_array($numP, $numPeople)) {
+		http_response_code(403); //Invalid Entry
+	} else if (!array_key_exists($building, $allBuildings)) {
+		http_response_code(403); //Invalid Entry (building)
+	} else if (!in_array($room, $_SESSION["buildings"][$building]['rooms'])) {
+		http_response_code(403); //Invalid Entry (room)
+	} else {
+		$utcStart = strtotime($localStart);
+		$startDate = date('Y-m-d', $utcStart);
+		$startTime = date('H:i:s', $utcStart);
+
+		$utcEnd = strtotime($localEnd);
+		$endDate = date('Y-m-d', $utcEnd);
+		$endTime = date('H:i:s', $utcEnd);
+
+		//Array to hold blocks 
+		$blocks = [];
+		$sth = $db->prepare("SELECT blockID, endTime FROM Blocks WHERE startTime = ?");
+		$sth->execute(array($startTime));
 		$rows = $sth->fetchAll();
 		
 		foreach($rows as $row) {
+			$block = $row['blockID'];
+			$blocks[] = $block;
 			$blockend = $row['endTime'];
 		}
+		
+		//Get all blocks for booking 
+		$totalB = 1;
+		while ($blockend != $endTime){
+			$block = $block + 1;
+			$totalB = $totalB + 1;
+			$blocks[] = $block;
+			
+			$sth = $db->prepare("SELECT endTime FROM Blocks WHERE blockID = ?");
+			$sth->execute(array($block));
+			$rows = $sth->fetchAll();
+			
+			foreach($rows as $row) {
+				$blockend = $row['endTime'];
+			}
+		}
+		
+		//create a booking 
+		
+		$sth = $db->prepare("INSERT INTO Bookings (UID, Reason, OtherDesc, AcademicYr, NumParticipants) VALUES (?,?,?,?,?)");	
+		$sth->execute(array($uID,$reason,$desc,$year,$numP));
+		
+		$bookingID = $db->lastInsertId();
+		//add to blocks to BookingSlots
+		$inserted = 0;
+		foreach ($blocks as $blockID){
+			$sth = $db->prepare("INSERT INTO BookingSlots VALUES (?, ?, ?, ?)");
+			$sth->execute(array($bookingID, $blockID, $startDate, $room));
+			$inserted = $inserted + $sth->rowCount();
+		}
+		
+		//If not all blocks inserted, delete booking
+		if ($totalB != $inserted) {
+			$sth = $db->prepare("DELETE FROM Bookingslots WHERE bookingID = ?");
+			$sth->execute(array($bookingID));
+			$sth = $db->prepare("DELETE FROM Bookings WHERE bookingID = ?");
+			$sth->execute(array($bookingID));
+			http_response_code(409); //conflict
+			$msg = "Your booking could not be completed. Please try making another booking.";
+		} else {
+			http_response_code(200); //success
+			$msg = 'Successfully booked: "' . $room . ', ' . $startDate . ' ' . $startTime . '-' . $endDate . ' ' . $endTime;
+		}
+			
+		//Send bookingID to front end
+		echo $bookingID;
 	}
-   	
-   	//create a booking 
-	
-   	$sth = $db->prepare("INSERT INTO Bookings (UID, Reason, OtherDesc, AcademicYr, NumParticipants) VALUES (?,?,?,?,?)");	
-	$sth->execute(array($UID,$Reason,$Desc,$year,$numP));
-	
-	$bookingID = $db->lastInsertId();
-	//add to blocks to BookingSlots
-	$inserted = 0;
-	foreach ($blocks as $blockID){
-		$sth = $db->prepare("INSERT INTO BookingSlots VALUES (?, ?, ?, ?)");
-		$sth->execute(array($bookingID, $blockID, $startDate, $Room));
-		$inserted = $inserted + $sth->rowCount();
-	}
-
-	//echo '$totalB: ' . $totalB . '<br>$inserted: ' . $inserted . '<br>'; 
-	
-	//If not all blocks inserted, delete booking
-	if ($totalB != $inserted) {
-		$sth = $db->prepare("DELETE FROM bookingslots WHERE BookingID = ?");
-		$sth->execute(array($bookingID));
-		$sth = $db->prepare("DELETE FROM bookings WHERE BookingID = ?");
-		$sth->execute(array($bookingID));
-		http_response_code(409); //conflict
-		$msg = "Your booking could not be completed. Please try making another booking.";
-	} else {
-		http_response_code(200); //success
-		$msg = 'Successfully booked: "' . $Room . ', ' . $startDate . ' ' . $startTime . '-' . $endDate . ' ' . $endTime;
-	}
-	
 	//Close the connection
 	$db = NULL;
-	
-	//Send bookingID to front end
-	echo $bookingID;
 ?>
