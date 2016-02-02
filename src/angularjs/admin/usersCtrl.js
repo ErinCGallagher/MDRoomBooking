a7836de0024c3a00fe6f9a121a048cb383db1e14
shannonklett <shannonklett@users.noreply.github.com>
2016-02-01 19:56:07 -0500

    Upload master alerts

--- src/angularjs/admin/usersCtrl.js
+++ src/angularjs/admin/usersCtrl.js
@@ -4,15 +4,26 @@ angular
 
 function UsersCtrl($scope, AdminUsersService) {
 
+	$scope.alerts = [];
+
+	$scope.closeAlert = function(index) {
+		$scope.alerts.splice(index, 1);
+	};
+
 	$scope.uploadList = function(uploadFile, dept) {
 		console.log("UsersCtrl ", dept);
 		AdminUsersService.uploadMasterList(uploadFile, dept)
 		.then(function(data){
-				alert(data.numUsersInDept, data.numUsersDeleted);
+				$msg = 'Users Uploaded. ' + data.numUsersInDept + ' users in ' + dept + '.';
+				$msg += data.numUsersDeleted + ' users deleted.';
+				alert = { type: 'info', msg: $msg};
+				$scope.alerts.push(alert);
 			},
 			function() {
-				alert("error");
+				alert = { type: 'danger', msg: 'Error: No users were uploaded.'};
+				$scope.alerts.push(alert);
 			});
+		
 	}
 
 };
