0fa9e9780c636b67d8a51e12029c393c6d3e5166
Lex-Luthor <lexi.flynn@queensu.ca>
2016-02-07 09:26:47 -0500

    getUsersInGroups

--- src/php/admin/getUsersInGroup.php
+++ src/php/admin/getUsersInGroup.php
@@ -1,30 +1,31 @@
 <?php
 
 	include('connection.php');
-
-	//Get post data stream 
-	$data = json_decode(file_get_contents("php://input"));
-	//Get parameters from 
-	$groupID = $data->groupId;
 	
-	//Get users from the database
-	$sth = $db->prepare('SELECT User.uID, firstName, lastName FROM Permission JOIN User ON Permission.uID = User.uID WHERE GroupID = ?');
-	$sth->execute(array($groupID));
-	$rows = $sth->fetchAll();
+		//Get post data stream 
+		$data = json_decode(file_get_contents("php://input"));
+		//Get parameters from 
+		$theGroupID = $data->groupId;
+		
+		//Get users from the database
+		$sth = $db->prepare('SELECT User.uID, firstName, lastName FROM Permission JOIN User ON Permission.uID = User.uID WHERE GroupID = ?');
+		$sth->execute(array($groupID));
+		$rows = $sth->fetchAll();
 	
-	$result = array();
+		$result = array();
 
-	//Put result in an array 
-	foreach($rows as $row) {
-		$result[] = $row;
-	}
+		//Put result in an array 
+		foreach($rows as $row) {
+			$result[] = $row;
+		}
 	
-	//Close the connection
-	$db = NULL;
+		//Close the connection
+		$db = NULL;
 
-	//Convert to json
-	$json = json_encode($result);
-	// echo the json string
-	echo $json;
+		//Convert to json
+		$json = json_encode($result);
+		// echo the json string
+		return $json;
    
 ?>
+
