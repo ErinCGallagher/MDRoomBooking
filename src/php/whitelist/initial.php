cb8e8b0d0aa30b98fc174b84b5bff313711b8f6d
Laura Brooks <11lmb23@queensu.ca>
2016-02-07 19:16:21 -0500

    Checks against building hours, two weeks in advance for students

--- src/php/whitelist/initial.php
+++ src/php/whitelist/initial.php
@@ -4,14 +4,12 @@ session_start();
 include('../connection.php');
 
 //Get all buildings
-$sth = $db->prepare("SELECT * FROM Building");
-$sth->execute();
-$rows = $sth->fetchAll();
-
 $allBuildings = array();
 
+$sth = $db->prepare("SELECT * FROM Building");
+$sth->execute();
 //Put result in an array 
-foreach($rows as $row){ 
+while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
 	$building = $row['buildingID'];
 	$allBuildings[$building] = array();
 	$allBuildings[$building]['openTime'] = $row['openTime'];
@@ -19,42 +17,51 @@ foreach($rows as $row){
 	
 	//get all rooms for building
 	$allBuildings[$building]['rooms'] = array();
-	$sth = $db->prepare("SELECT roomID FROM Rooms WHERE building = ?");
-	$sth->execute(array($building));
-	$rooms = $sth->fetchAll();
-	foreach($rooms as $room){ 
+	$sth2 = $db->prepare("SELECT roomID FROM Rooms WHERE building = ?");
+	$sth2->execute(array($building));
+	while($room = $sth2->fetch(PDO::FETCH_ASSOC)) {
 		$allBuildings[$building]['rooms'][] = $room['roomID'];
 	}	
 }
 
 $_SESSION["buildings"] = $allBuildings;
-$class = "admin"; //hard codded for lacal env
-$user = "11ecg5"; //hard codded for local env
-/*
+
 //Check if user can book
-$user = $_SERVER["HTTP_QUEENSU_NETID"];
+//$user = $_SERVER["HTTP_QUEENSU_NETID"];
+$user = "11lmb23";
 $canBook = False;
 $sth = $db->prepare("SELECT * FROM Master WHERE uID = ?");
 $sth->execute(array($user));
-$rows = $sth->fetchAll();
-foreach ($rows as $row){
+//Loop through each returned row 
+while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
 	$canBook = True;
 }
 if ($canBook) {
-	$sth = $db-> prepare("SELECT * FROM User WHERE uID = ?");
+	$sth = $db-> prepare("SELECT class FROM User WHERE uID = ?");
 	$sth->execute(array($user));
-	$rows = $sth->fetchAll();
-	foreach($rows as $row){
+	while($row = $sth->fetch(PDO::FETCH_ASSOC)) {
 		$class = $row['class'];
 	}
 } else {
-	$class = "none";
+	//class is NonBooking
+	$class = "NonBooking";
+	//unless it is in admin.txt then class is Admin
+	$file = fopen("admin.txt", "r") or die("Unable to open file!");
+	// Output one line until end-of-file
+	while(!feof($file)) {
+		$check = fgets($file);
+		$check = trim($check);
+		if ($check == $user) {
+			$class = "Admin";
+		}
+	}
+	fclose($file);
 }
-*/
 
 $db = NULL;
 $data = array();
 $data["allBuildings"] = $allBuildings;
+$_SESSION["class"] = $class;
 $data["class"] = $class;
 $data["netID"] = $user;
 
