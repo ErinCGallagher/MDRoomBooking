5eca5ab6208db367efb5f735f2d01e3f92fdd36d
shannonklett <shannonklett@users.noreply.github.com>
2016-02-07 21:07:58 -0500

    Using transaction in addUsers

--- src/php/admin/addUsers.php
+++ src/php/admin/addUsers.php
@@ -57,9 +57,10 @@
 	//Get parameters from frontend
 	$groupID = $_POST['groupID'];
 
-	// reads in file and stores contents in $contents
-	// each element is the userID of a user to be added to the group
+	// reads in file and returns an array of contents, one for each non-empty line
+	// each element should be the userID of a user to be added to the group
 	require '../uploadFile.php';
+	$fileLines = processFile();
 
 	// array of group info containing (addHrsType, hours, hasBookingDurationRestriction, startDate, endDate)
 	$groupInfo = getGroupInfo($db, $groupID);
@@ -73,27 +74,25 @@
 	$curWeekUpdateString = "";
 	$nextWeekUpdateString = "";
 	$restUpdateString = "";
-
-	$usersAdded = array();
 	
-	//Arrays to hold query values
+	//Array to hold users being added (error-free)
+	$usersAddedArray = array();
+
+	//Array holding users + info to be added to permission table
 	$insertArray = array();
-	$curWeekUpdateArray = array();
-	$nextWeekUpdateArray = array();
-	$restUpdateArray = array();
 
 	//Arrays to hold error users
 	$alreadyInGroupArray = array();
 	$notInMasterArray = array();
 	
-	foreach ($contents as $user) {
+	foreach ($fileLines as $user) {
 		
 		if (!userInGroup($db, $user, $groupID)) {
 			// user is NOT already in group, continue with addition
 
 			if (userInMasterList($db, $user)) {
 
-				array_push($usersAdded, $user);
+				array_push($usersAddedArray, $user);
 
 				// add user to permissions table
 				// specialHrs = 0 if the group has weekly hours
@@ -104,20 +103,17 @@
 					// if group has weekly hours that are currently active, 
 					// they should be immediately given to the user
 					$curWeekUpdateString .= "uID = ? OR ";
-					array_push($curWeekUpdateArray, $user);
 				}
 
 				if(groupHasNextWeekHours($db, $groupInfo)) {
 					// if group has hours that are active next week, 
 					// they should be immediately given to the user
 					$nextWeekUpdateString .= "uID = ? OR ";
-					array_push($nextWeekUpdateArray, $user);
 				}
 
 				// deal with booking restriction
 				if(strcmp($groupInfo['hasBookingDurationRestriction'], 'No') == 0) {
 					$restUpdateString .= "uID = ? OR ";
-					array_push($restUpdateArray, $user);
 				}
 				
 				
@@ -138,47 +134,56 @@
 	$nextWeekUpdateString = chop($nextWeekUpdateString, " OR ");
 	$restUpdateString = chop($restUpdateString, " OR ");
 
-	//Insert Permissions
-	$insertQuery = "INSERT INTO Permission (uID, groupID, specialHrs) VALUES $insertString";
-	$insertStmt = $db->prepare($insertQuery);
-	$insertStmt->execute($insertArray);
 
-	if ($insertStmt) {
-		// insert successful
+	// update DB
+	try {
+		$db->setAttribute (PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+		$db->beginTransaction();
+
+		//Insert Permissions
+		$insertQuery = "INSERT INTO Permission (uID, groupID, specialHrs) VALUES $insertString";
+		$insertStmt = $db->prepare($insertQuery);
+		$insertStmt->execute($insertArray);
 
 		$weekHours = $groupInfo['hours'];
 		//update user table with current active weekly hours
 		if(groupHasCurWeekHours($db, $groupInfo)) {
 			$curWeekUpdateQuery = "UPDATE User SET curWeekHrs = curWeekHrs + $weekHours WHERE $curWeekUpdateString";
 			$curWeekUpdateStmt = $db->prepare($curWeekUpdateQuery);
-			$curWeekUpdateStmt->execute($curWeekUpdateArray);
+			$curWeekUpdateStmt->execute($usersAddedArray);
 		}
 
 		//update user table with active weekly hours for next week
 		if(groupHasNextWeekHours($db, $groupInfo)) {
 			$nextWeekUpdateQuery = "UPDATE User SET nextWeekHrs = nextWeekHrs + $weekHours WHERE $nextWeekUpdateString";
 			$nextWeekUpdateStmt = $db->prepare($nextWeekUpdateQuery);
-			$nextWeekUpdateStmt->execute($nextWeekUpdateArray);
+			$nextWeekUpdateStmt->execute($usersAddedArray);
 		}
 
 		if(strcmp($groupInfo['hasBookingDurationRestriction'], 'No') == 0) {
 			$restUpdateQuery = "UPDATE User SET hasBookingDurationRestriction = 'No' WHERE $restUpdateString";
 			$restUpdateStmt = $db->prepare($restUpdateQuery);
-			$restUpdateStmt->execute($restUpdateArray);
+			$restUpdateStmt->execute($usersAddedArray);
 		}
 
-	} else {
-		http_response_code(500); //Internal Server Error
-	}
+		$db->commit();
 
-	$result = array();
-	$result["addedUsers"] = $usersAdded; 
-	$result["usersAlreadyInGroup"] = $alreadyInGroupArray;
-	$result["usersNotInMaster"] = $notInMasterArray; 
-	
-	//Convert to json
-	$json = json_encode($result);
-	// echo the json string
-	echo $json;
+		$result = array();
+		$result["addedUsers"] = $usersAddedArray; 
+		$result["usersAlreadyInGroup"] = $alreadyInGroupArray;
+		$result["usersNotInMaster"] = $notInMasterArray; 
+		
+		//Convert to json
+		$json = json_encode($result);
+		// echo the json string
+		echo $json;
+	} catch (Exception $e) { 
+		http_response_code(500); //Internal Server Error
+	    if (isset ($db)) {
+	       $db->rollback ();
+	       echo "Error:  " . $e; 
+	    }
+	}
 
 ?>
--- src/php/uploadFile.php
+++ src/php/uploadFile.php
@@ -1,32 +1,36 @@
 <?php
 
-	$uploadOk = 1;
-	// $_FILES is a global variable that gets the files from HTTP POST
-	$fileType = pathinfo($_FILES["fileToUpload"]["name"],PATHINFO_EXTENSION);
+	function processFile(){
+		$uploadOk = 1;
+		// $_FILES is a global variable that gets the files from HTTP POST
+		$fileType = pathinfo($_FILES["fileToUpload"]["name"],PATHINFO_EXTENSION);
+		
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 500000) {
+		    echo "Sorry, your file is too large.";
+		    $uploadOk = 0;
+		}
+		// Check file format
+		if($fileType != "csv") {
+		    echo "File must be a .csv file.";
+		    $uploadOk = 0;
+		}
+
+		// Check that we are processig the correct file (not being hacked)
+		if (!is_uploaded_file(realpath($_FILES['fileToUpload']['tmp_name']))) {
+	    	echo "Not processing expected file.";
+		    $uploadOk = 0;
+	    }
+
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+		    http_response_code(500);
+			die( "Error: File could not be uploaded."); // TODO: Check if this is the best way to handle error.
+		// if everything is ok, read in file to $contents
+		} else {
+			// return an array with an element for each line of the file (newlines removed)
+	    	return file($_FILES["fileToUpload"]["tmp_name"], FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+		}
+	}
 	
-	// Check file size
-	if ($_FILES["fileToUpload"]["size"] > 500000) {
-	    echo "Sorry, your file is too large.";
-	    $uploadOk = 0;
-	}
-	// Check file format
-	if($fileType != "csv") {
-	    echo "File must be a .csv file.";
-	    $uploadOk = 0;
-	}
-
-	// Check that we are processig the correct file (not being hacked)
-	if (!is_uploaded_file(realpath($_FILES['fileToUpload']['tmp_name']))) {
-    	echo "Not processing expected file.";
-	    $uploadOk = 0;
-    }
-
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-	    echo "Sorry, your file was not uploaded.";
-	// if everything is ok, read in file to $contents
-	} else {
-		// $contents is an array with an element for each line of the file (newlines removed)
-    	$contents=file($_FILES["fileToUpload"]["tmp_name"], FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-	}
 ?>
