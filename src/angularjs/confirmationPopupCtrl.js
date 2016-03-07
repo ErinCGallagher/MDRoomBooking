3ea02712d3dbfe21d4155facd6df4fd3bf204935
shannonklett <shannonklett@users.noreply.github.com>
2016-03-06 22:50:28 -0500

    Confirmation popup for deleting users from group (either single user or all)

---
+++ src/angularjs/confirmationPopupCtrl.js
@@ -0,0 +1,18 @@
+angular
+.module('mainApp')
+.controller('ConfirmationPopupCtrl', ConfirmationPopupCtrl);
+
+function ConfirmationPopupCtrl ($scope, $uibModalInstance, submitFunction, submitData, msg) {
+
+	$scope.msg = msg; // shown in modal body, can contain html tags
+
+	$scope.submit = function () {
+		submitFunction(submitData); 
+		$uibModalInstance.close();
+	};
+
+
+	$scope.cancel = function () {
+		$uibModalInstance.dismiss('cancel');
+	};
+};
