d274974cb566b69abfcaf5b255c3a9661099f6f3
shannonklett <shannonklett@users.noreply.github.com>
2016-03-09 14:37:43 -0500

    Revert "New saveFileAs function, saves uploaded user list"
    This reverts commit 42914dc19fcafdca939fce39bb52c8467014f602.

--- src/php/uploadFile.php
+++ src/php/uploadFile.php
@@ -32,13 +32,5 @@
 	    	return file($_FILES["fileToUpload"]["tmp_name"], FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 		}
 	}
-
-	function saveFileAs($name) {
-		$fileContents = processFile();
-		//save file to server
-		move_uploaded_file( $_FILES['fileToUpload']['tmp_name'], $name); 
-		return $fileContents;
-
-	}
 	
 ?>
