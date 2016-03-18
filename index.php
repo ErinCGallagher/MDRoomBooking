4f93436de064ee803c1e6f2cffe3ec19f5b820f9
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2016-03-18 13:30:18 -0400

    Erin Fixed What she Broke

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="src/html_css/search.css"/>
         <link rel="stylesheet" href="src/html_css/myBookings.css"/>
         <link rel="stylesheet" href="src/html_css/users.css"/>
+         <link rel="stylesheet" href="src/html_css/keyList.css"/>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -32,16 +33,23 @@
         <script type="text/javascript" src="src/angularjs/appCtrl.js"></script>
         <script type="text/javascript" src="src/angularjs/bookings/calendarCtrl.js"></script>
         <script type="text/javascript" src="src/angularjs/bookings/searchCtrl.js"></script>
-        <script type="text/javascript" src="src/angularjs/bookings/myBookingsCtrl.js"></script>
-        <script type="text/javascript" src="src/angularjs/bookings/makeBookingPopupCtrl.js"></script>
-        <script type="text/javascript" src="src/angularjs/bookings/viewBookingPopupCtrl.js"></script>
         <script type="text/javascript" src="src/angularjs/admin/groupsCtrl.js"></script>
         <script type="text/javascript" src="src/angularjs/admin/usersCtrl.js"></script>
         <script type="text/javascript" src="src/angularjs/loginCtrl.js"></script>
-        <script type="text/javascript" src="src/angularjs/admin/viewUsersPopupCtrl.js"></script>
+        
         <script type="text/javascript" src="src/angularjs/signOutCtrl.js"></script>
+        
+
+        <!-- Modal Controllers-->
+        <script type="text/javascript" src="src/angularjs/bookings/myBookingsCtrl.js"></script>
+        <script type="text/javascript" src="src/angularjs/bookings/makeBookingPopupCtrl.js"></script>
+        <script type="text/javascript" src="src/angularjs/bookings/viewBookingPopupCtrl.js"></script>
+        <script type="text/javascript" src="src/angularjs/bookings/bookingConfirmationPopupCtrl.js"></script>
+        <script type="text/javascript" src="src/angularjs/bookings/recurringBookingConfirmationPopupCtrl.js"></script>
         <script type="text/javascript" src="src/angularjs/bookings/confirmCancelCtrl.js"></script>
         <script type="text/javascript" src="src/angularjs/confirmationPopupCtrl.js"></script>
+        <script type="text/javascript" src="src/angularjs/admin/keyListCtrl.js"></script>
+        <script type="text/javascript" src="src/angularjs/admin/viewUsersPopupCtrl.js"></script>
 
         <!-- Services -->
         <script type="text/javascript" src="src/angularjs/commService.js"></script>
@@ -55,6 +63,7 @@
         <script type="text/javascript" src="src/angularjs/bookings/searchService.js"></script>
         <script type="text/javascript" src="src/angularjs/bookings/myBookingsService.js"></script>
         <script type="text/javascript" src="src/angularjs/confirmationPopupService.js"></script>
+        <script type="text/javascript" src="src/angularjs/admin/adminKeyService.js"></script>
 
         <!-- Bootstrap/Modal Stuff -->
         <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" >
@@ -130,6 +139,7 @@
                       <ul class="dropdown-menu" >
                         <li ng-class="{ active: $route.current.activesubtab == 'user'}"><a href="#/admin/user">{{UserText}}</a></li>
                         <li ng-class="{ active: $route.current.activesubtab == 'groups'}"><a href="#/admin/groups">{{GroupsText}}</a></li>
+                        <li ng-class="{ active: $route.current.activesubtab == 'keyList'}"><a href="#/admin/keyList">{{KeyText}}</a></li>
                       </ul>
                     </li>
                     <li ng-class="{ active: $route.current.activetab == 'other'}">
