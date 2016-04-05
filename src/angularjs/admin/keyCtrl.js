9b8ca2bec6d01734493fb5c805573c8abee47d3e
shannonklett <shannonklett@users.noreply.github.com>
2016-04-05 13:49:00 -0400

    Deleting unused files

--- src/angularjs/admin/keyCtrl.js
+++
@@ -1,8 +0,0 @@
-angular
-.module('mainApp')
-.controller('KeyListCtrl', KeyLisyCtrl);
-
-function UsersCtrl($scope, $uibModal, AdminUsersService) {
-	$scope.pageClass = 'keyList';  //used to change pages in index.php
-
-}
