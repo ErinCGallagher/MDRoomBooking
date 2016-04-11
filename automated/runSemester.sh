34b09e6454e36535e0daf539fbf4723b6b69f915
shannonklett <shannonklett@users.noreply.github.com>
2016-04-11 03:32:04 -0400

    Finished crontab

--- automated/runSemester.sh
+++ automated/runSemester.sh
@@ -1,5 +1,1 @@
 /opt/rh/rh-php56/root/usr/bin/php /home/users/DMRoomBooking/wwws/artsci/DMRoomBooking/automated/semester.php
-# date +"%D %H:%M" >> semester.log
-
-# now="$(date +'%d/%m/%Y')"
-# printf "Current date in dd/mm/yyyy format %s\n" "$now"
